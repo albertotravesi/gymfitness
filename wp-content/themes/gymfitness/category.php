c7701abef0355d277654f87d8479e9699f41219c
Alberto Travesi <albertotravesi7@gmail.com>
2022-12-16 15:28:43 -0600

    Crear Blog - Crear templeate para las categorias

--- wp-content/themes/gymfitness/category.php
+++ wp-content/themes/gymfitness/category.php
@@ -1,6 +1,15 @@
 <?php get_header(); ?>
 
     <main class="pagina seccion no-sidebars contenedor">
+
+        <?php 
+            $categoria = get_queried_object(); 
+        ?>
+
+        <h2 class="text-center texto-primario">
+            CATEGORIA: <?php echo $categoria->name;?>
+        </h2>
+        
         <ul class="listado-blog">
             <?php get_template_part('template-parts/loop-blog', 'blog') ?>
         </ul>  
